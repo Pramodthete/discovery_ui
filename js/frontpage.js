dd4c10cdc7f0f350aec4ea0c922bc3c4e1455861
Pramod <pthetepatil@gmail.com>
2024-05-02 15:23:23 +0530

    Discovery UI: Front Page completed

---
+++ js/frontpage.js
@@ -0,0 +1,8 @@
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+// var texts = ["Pay a Grand Golf & Ocean Club", "Naples Beach Club", "Driftwood golf & ranch club","Homewood mountain Beach Club","Zapotal Golf & Beach Club","Taymouth Castle"];
+
