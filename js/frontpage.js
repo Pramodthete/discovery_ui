8337d127d629ff11882c20714f35bc245c2c838c
Pramod <pthetepatil@gmail.com>
2024-05-03 21:45:28 +0530

    Second and third page: used some animation

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,7 +1,15 @@
 
 function myFunction(x) {
     x.classList.toggle("change");
+    $('.header').hide();
+        $('.home').hide();
+        $('.menu').css('background',"red").show().slideDown(); 
+        x.classList.show();   
 }
+
+$('.menu').ready(function(){
+    $('.menu').hide();
+})
 
 
 // var texts = ["Pay a Grand Golf & Ocean Club", "Naples Beach Club", "Driftwood golf & ranch club","Homewood mountain Beach Club","Zapotal Golf & Beach Club","Taymouth Castle"];
